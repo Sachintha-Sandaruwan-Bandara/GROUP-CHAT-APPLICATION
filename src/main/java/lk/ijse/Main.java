fcf733b996ce2cb4425e6444a1a23e6849e4938f
Sachintha-Sandaruwan-Bandara <sachib843@gmail.com>
2024-01-14 21:45:42 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/Main.java
@@ -0,0 +1,11 @@
+package lk.ijse;
+/* 
+    @author Sachi_S_Bandara
+    @created $DATE - $TIME 
+*/
+    
+    public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
