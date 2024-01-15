020e08cc8708cb775070bc8c73f4c6e5b9c95529
Sachintha-Sandaruwan-Bandara <sachib843@gmail.com>
2024-01-15 10:15:44 +0530

    login page done

--- src/main/java/lk/ijse/GROUP_CHAT_APPLICATION/controller/LoginPageFormController.java
+++ src/main/java/lk/ijse/GROUP_CHAT_APPLICATION/controller/LoginPageFormController.java
@@ -4,5 +4,19 @@ package lk.ijse.GROUP_CHAT_APPLICATION.controller;
     @created 1/15/2024 - 10:02 AM 
 */
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 public class LoginPageFormController {
+
+        @FXML
+        private TextField txtUserName;
+
+        @FXML
+        void btnLoginOnAction(ActionEvent event) {
+
+        }
+
 }
+
+
